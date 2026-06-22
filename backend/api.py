35b0d2afcc77b229b1280c0e75c45f36b9bfc3af
unknown <ca.tumnat@gmail.com>
2026-06-22 18:29:41 +0700

    Add pdf extraction

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import fitz
 app = FastAPI()
 
 @app.get("/")
@@ -17,4 +17,17 @@ def hi(name: str):
     return {
         "message": f"Hi {name}",        
         "alert": "This is an alert message"
+    }
+
+@app.get("/readpdf")
+def read_pdf():
+    doc = fitz.open("thantam_tumnat_resume.pdf")
+
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+
+    return {
+        "content": text
     }
